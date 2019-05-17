4f49afff74a6053c120d7acb316142e09c33b636
Boris LECLERE <Borisleclere@gmail.com>
2019-05-17 18:13:24 +0200

    Add skeleton Add HomeController + AllCats function Add Index view + css + js
    Load AllCast on windows load

---
+++ CatMash/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CatMash.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<Cat[]> AllCats()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://latelier.co/data/cats.json");
+
+            //will throw an exception if not successful
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            var dico = JsonConvert.DeserializeObject<Dictionary<string, Cat[]>>(content);
+
+            return dico["images"];
+        }
+    }
+
+    public class Cat
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ CatMash/wwwroot/js/Index.js
@@ -0,0 +1,37 @@
+﻿window.onload = function() {
+    console.log("Loaded");
+
+    $.ajax({
+       url : 'http://localhost:5000/Home/AllCats', // Le nom du script a changé, c'est send_mail.php maintenant !
+       type : 'GET', // Le type de la requête HTTP, ici devenu POST
+       dataType : 'json',
+       success : function(data, statut){
+           console.log("Success");
+           console.log(data);
+
+           var cats = data;
+
+           var leftCat = cats[0];
+           var rightCat = cats[1];
+
+           //Left Cat
+           setCatImage("leftCatImg", leftCat.url, leftCat.id);
+
+           //Right Cat
+           setCatImage("rightCatImg", rightCat.url, rightCat.id);
+       },
+
+       error : function(resultat, statut, erreur){
+           console.log("Error");
+       },
+
+       complete : function(resultat, statut){
+           console.log("Completed");
+       }
+    });
+};
+
+function setCatImage(imageId, src, alt){
+    document.getElementById(imageId).src = src;
+    document.getElementById(imageId).alt = alt;
+}
