0230fe67f334f906f85e7608dd83c32f1db51446
Boris LECLERE <Borisleclere@gmail.com>
2019-12-08 15:32:45 +0100

    Add init migration

--- DataStore/EF/CatMash.DataStore.EF/CatMashDBContext+MigrationId.cs
+++ DataStore/EF/CatMash.DataStore.EF/CatMashDBContext+MigrationId.cs
@@ -4,6 +4,6 @@ namespace CatMash.DataStore.EF
     public partial class CatMashDBContext
     {
         /** Auto generated using migration script */
-public static readonly string MigrationId = "41256B3B-F72D-4EA8-8DDE-57432266CB5C";
+public static readonly string MigrationId = "4AD23D26-AD24-4E11-9F3F-8051BD1DAEBA";
     }
 }
---
+++ DataStore/EF/SQLite/CatMash.DataStore.EF.SQLite/Migrations/20191208143226_AutoMigrate_bleclereperso_15H32M21S_12-08-19 (Init).cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatMash.DataStore.EF.SQLite.Migrations
+{
+    public partial class AutoMigrate_bleclereperso_15H32M21S_120819Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    URL = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    ImageId = table.Column<int>(nullable: true),
+                    NbMash = table.Column<int>(nullable: false),
+                    Rate = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Cats_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mash",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    LeftCatName = table.Column<string>(nullable: true),
+                    RightCatName = table.Column<string>(nullable: true),
+                    WinnerCatName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mash", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mash_Cats_LeftCatName",
+                        column: x => x.LeftCatName,
+                        principalTable: "Cats",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Mash_Cats_RightCatName",
+                        column: x => x.RightCatName,
+                        principalTable: "Cats",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Mash_Cats_WinnerCatName",
+                        column: x => x.WinnerCatName,
+                        principalTable: "Cats",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cats_ImageId",
+                table: "Cats",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mash_LeftCatName",
+                table: "Mash",
+                column: "LeftCatName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mash_RightCatName",
+                table: "Mash",
+                column: "RightCatName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mash_WinnerCatName",
+                table: "Mash",
+                column: "WinnerCatName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mash");
+
+            migrationBuilder.DropTable(
+                name: "Cats");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
