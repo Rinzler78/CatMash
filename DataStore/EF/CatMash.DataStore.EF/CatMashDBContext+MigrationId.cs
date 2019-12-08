79f7d33b220aef8f4379a0b82b9f7651a08e81a4
Boris LECLERE <Borisleclere@gmail.com>
2019-12-08 15:11:11 +0100

    Add Migration Helper

--- DataStore/EF/CatMash.DataStore.EF/CatMashDBContext+MigrationId.cs
+++ DataStore/EF/CatMash.DataStore.EF/CatMashDBContext+MigrationId.cs
@@ -4,6 +4,6 @@ namespace CatMash.DataSTore.EF
     public partial class CatMashDBContext
     {
         /** Auto generated using migration script */
-public static readonly string MigrationId = "5A01C753-68A1-408B-8707-FD693C7B1AEF";
+public static readonly string MigrationId = "41256B3B-F72D-4EA8-8DDE-57432266CB5C";
     }
 }
